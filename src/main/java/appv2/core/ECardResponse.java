9d0afa43d9a05397ef11bd74de44c1515047bc5a
mzosch <magnus.zoeschinger@zoeschinger.de>
2023-10-29 18:51:38 +0100

    added javadocs for core enums

--- src/main/java/appv2/core/ECardResponse.java
+++ src/main/java/appv2/core/ECardResponse.java
@@ -1,10 +1,38 @@
 package appv2.core;
 
+
+/**
+ * <p>Represents the possible responses to a card being played.</p>
+ */
 public enum ECardResponse {
+    /**
+     * <p>Card was played successfully. The caller will need to figure
+     * out how to update the affected players' hands accordingly.</p>
+     */
     RC_OK,
+    /**
+     * <p>Card was not played successfully. The caller will need to revert
+     * changed variables accordingly.</p>
+     */
     RC_ERR,
+    /**
+     * <p>Card was played successfully. The player who played the card is
+     * knocked out of the round. The caller won't need to do anything else.</p>
+     */
     RC_OK_KNOCKED_OUT,
+    /**
+     * <p>Card was played successfully. The caller will have to call the callback
+     * method on this card with a valid player name.</p>
+     */
     RC_CHOOSE_ANY_PLAYER,
+    /**
+     * <p>Card was played successfully. The caller will have to call the callback
+     * method on this card with a valid player name that is not themself.</p>
+     */
     RC_CHOOSE_ANY_PLAYER_SELF_EXCLUDED,
+    /**
+     * <p>Card was played successfully. The caller will have to call the callback
+     * method on this card with a valid player name and a valid integer.</p>
+     */
     RC_CHOOSE_ANY_PLAYER_SELF_EXCLUDED_WITH_INTEGER,
 }
--- src/main/java/appv2/core/EGameModeState.java
+++ src/main/java/appv2/core/EGameModeState.java
@@ -1,7 +1,23 @@
 package appv2.core;
 
+
+/**
+ * <p>Represents the possible states a game can be in.</p>
+ */
 public enum EGameModeState {
+    /**
+     * <p>Game is running. The caller will have to render the turn of
+     * the current active Player Controller.</p>
+     */
     GAME_RUNNING,
+    /**
+     * <p>A round has ended. The caller will have to show
+     * a round ended screen.</p>
+     */
     ROUND_ENDED,
+    /**
+     * <p>The game has ended. The caller will have to show
+     * a game ended screen.</p>
+     */
     GAME_ENDED,
 }
